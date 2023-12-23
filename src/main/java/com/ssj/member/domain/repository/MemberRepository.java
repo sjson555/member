0026f4cb0eab184a5c75fb9fa5c936a4ac026f88
sungjun son <o_nj_u@naver.com>
2023-12-23 20:11:03 +0900

    20231223 signup / login / logout

---
+++ src/main/java/com/ssj/member/domain/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.ssj.member.domain.repository;
+
+import com.ssj.member.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String userEmail);
+}
