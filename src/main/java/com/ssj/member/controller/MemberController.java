f9dd25cab5d754fc7749e519960d414021e3bc4d
sungjun son <o_nj_u@naver.com>
2023-12-26 00:09:57 +0900

    20231226 회원가입유효성검사

--- src/main/java/com/ssj/member/controller/MemberController.java
+++ src/main/java/com/ssj/member/controller/MemberController.java
@@ -4,8 +4,13 @@ import com.ssj.member.service.MemberService;
 import com.ssj.member.dto.MemberDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
@@ -15,56 +20,66 @@ public class MemberController {
     // 메인 페이지
     @GetMapping("/")
     public String index() {
-        return "/common/index";
+        return "common/index";
     }
 
     // 회원가입 페이지
     @GetMapping("/user/signup")
     public String dispSignup() {
-        return "/member/signup";
+        return "member/signup";
     }
 
     // 회원가입 처리
     @PostMapping("/user/signup")
-    public String execSignup(MemberDto memberDto) {
-        memberService.joinUser(memberDto);
+    public String execSignup(@Valid MemberDto memberDto, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            // 회원가입 실패시, 입력 데이터를 유지
+            model.addAttribute("memberDto", memberDto);
 
+            // 유효성 통과 못한 필드와 메시지를 핸들링
+            Map<String, String> validatorResult = memberService.validateHandling(errors);
+            for (String key : validatorResult.keySet()) {
+                model.addAttribute(key, validatorResult.get(key));
+            }
+            return "member/signup";
+        }
+        memberService.joinUser(memberDto);
         return "redirect:/user/login";
     }
 
     // 로그인 페이지
     @GetMapping("/user/login")
     public String dispLogin() {
-        return "/member/login";
+        return "member/login";
     }
 
     // 로그인 결과 페이지
     @GetMapping("/user/login/result")
     public String dispLoginResult() {
-        return "/member/loginSuccess";
+        return "member/loginSuccess";
     }
 
     // 로그아웃 결과 페이지
     @GetMapping("/user/logout/result")
     public String dispLogout() {
-        return "/member/logout";
+        return "member/logout";
     }
 
     // 접근 거부 페이지
     @GetMapping("/user/denied")
     public String dispDenied() {
-        return "/member/denied";
+        return "member/denied";
     }
 
     // 내 정보 페이지
     @GetMapping("/user/info")
     public String dispMyInfo() {
-        return "/member/myinfo";
+        return "member/myinfo";
     }
 
     // 어드민 페이지
     @GetMapping("/admin")
     public String dispAdmin() {
-        return "/member/admin";
+        return "member/admin";
     }
 }
--- src/main/java/com/ssj/member/service/MemberService.java
+++ src/main/java/com/ssj/member/service/MemberService.java
@@ -14,15 +14,28 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
 public class MemberService implements UserDetailsService {
     private MemberRepository memberRepository;
+
+
+    // 회원가입 시, 유효성 체크
+    public Map<String, String> validateHandling(Errors errors) {
+        Map<String, String> validatorResult = new HashMap<>();
+
+        for (FieldError error : errors.getFieldErrors()) {
+            String validKeyName = String.format("valid_%s", error.getField());
+            validatorResult.put(validKeyName, error.getDefaultMessage());
+        }
+
+        return validatorResult;
+    }
 
     @Transactional
     public Long joinUser(MemberDto memberDto) {
@@ -36,7 +49,7 @@ public class MemberService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
         Optional<MemberEntity> userEntityWrapper = memberRepository.findByEmail(userEmail);
-        MemberEntity userEntity = userEntityWrapper.get();
+        MemberEntity userEntity = userEntityWrapper.orElseThrow(() -> new UsernameNotFoundException("해당 이메일로 가입된 유저가 없습니다: " + userEmail));
 
         List<GrantedAuthority> authorities = new ArrayList<>();
 
