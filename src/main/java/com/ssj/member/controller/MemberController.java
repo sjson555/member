c6577a0205ba1cf6f0a0d1e5174286b44ad52cf8
sungjun son <o_nj_u@naver.com>
2024-01-08 23:39:47 +0900

    20240108 로그인성공, 로그아웃성공 -> index페이지로

--- src/main/java/com/ssj/member/controller/MemberController.java
+++ src/main/java/com/ssj/member/controller/MemberController.java
@@ -63,13 +63,14 @@ public class MemberController {
     // 로그인 결과 페이지
     @GetMapping("/user/login/result")
     public String dispLoginResult() {
-        return "member/loginSuccess";
+        return "common/index";
     }
 
     // 로그아웃 결과 페이지
     @GetMapping("/user/logout/result")
     public String dispLogout() {
-        return "member/logout";
+        return "common/index";
+
     }
 
     // 접근 거부 페이지
